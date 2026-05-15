13723f1b6dce12d18d12549e1693a8d1d671793c
okanberhoglu <okan.berhoglu@metu.edu.tr>
2026-05-15 15:36:40 +0300

    Menu bug is fixed.

--- JS/menuOpen.js
+++ JS/menuOpen.js
@@ -1,5 +1,6 @@
 const navbar = document.getElementsByClassName("navbar");
 const menu = document.getElementsByClassName("menu");
+const menuSectionLinks = document.querySelectorAll(".menu-list a[href^='#']");
 const mobileMenuQuery = window.matchMedia("(max-width: 965px)");
 let scrollPosition = 0;
 
@@ -11,30 +12,58 @@ function lockBodyScroll() {
   document.body.classList.add("menu-scroll-locked");
 }
 
-function unlockBodyScroll() {
+function jumpToScrollPosition(position) {
+  const originalScrollBehavior = document.documentElement.style.scrollBehavior;
+
+  document.documentElement.style.scrollBehavior = "auto";
+  window.scrollTo(0, position);
+  document.documentElement.style.scrollBehavior = originalScrollBehavior;
+}
+
+function unlockBodyScroll(position = scrollPosition) {
   if (!document.body.classList.contains("menu-scroll-locked")) return;
 
   document.body.classList.remove("menu-scroll-locked");
   document.body.style.top = "";
-  window.scrollTo(0, scrollPosition);
+  jumpToScrollPosition(position);
+}
+
+function closeMenu(position = scrollPosition) {
+  navbar[0].classList.remove("active");
+  menu[0].classList.remove("activeMenu");
+  unlockBodyScroll(position);
+}
+
+function openMenu() {
+  navbar[0].classList.add("active");
+  menu[0].classList.add("activeMenu");
+  lockBodyScroll();
 }
 
 function menuOpen() {
   if (navbar[0].classList.contains("active")) {
-    navbar[0].classList.remove("active");
-    menu[0].classList.remove("activeMenu");
-    unlockBodyScroll();
-  } else {
-    navbar[0].classList.add("active");
-    menu[0].classList.add("activeMenu");
-    lockBodyScroll();
+    closeMenu();
+    return;
   }
+
+  openMenu();
 }
+
+menuSectionLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    const target = document.querySelector(link.hash);
+
+    if (!target) return;
+
+    event.preventDefault();
+    event.stopPropagation();
+    closeMenu(target.offsetTop);
+    history.pushState(null, "", link.hash);
+  });
+});
 
 mobileMenuQuery.addEventListener("change", () => {
   if (!mobileMenuQuery.matches) {
-    navbar[0].classList.remove("active");
-    menu[0].classList.remove("activeMenu");
-    unlockBodyScroll();
+    closeMenu();
   }
 });
